0558a5c506f3d3b07707344447fe44ada8b1721f
wizzardo <wizzardobox@gmail.com>
2015-10-04 19:57:46 +0300

    start implementing simple rtorrent client

---
+++ src/main/java/com/wizzardo/jrt/RTorrentClient.java
@@ -0,0 +1,117 @@
+package com.wizzardo.jrt;
+
+import com.wizzardo.tools.collections.CollectionTools;
+import com.wizzardo.tools.xml.Node;
+import com.wizzardo.tools.xml.XmlParser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by wizzardo on 03.10.15.
+ */
+public class RTorrentClient {
+
+    private String host;
+    private int port;
+
+    public RTorrentClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public List<TorrentInfo> getTorrents() {
+        String response = executeRequest(new ScgiClient.XmlRpc("d.multicall", "main",
+                "d.name=",
+                "d.hash=",
+                "d.size_bytes=",
+                "d.bytes_done=",
+                "d.get_down_rate=",
+                "d.get_up_rate=",
+                "d.get_complete=",
+                "d.is_open=",
+                "d.is_hash_checking=",
+                "d.get_state=",
+                "d.get_peers_complete=",
+                "d.get_peers_accounted=",
+                "d.get_up_total="
+        ));
+        XmlParser parser = new XmlParser();
+        Node methodResponse = parser.parse(response);
+//        System.out.println("response: " + methodResponse.toXML(true));
+//        List<TorrentInfo> torrents = CollectionTools.collect(methodResponse.getAll("params/param/value/array/data/value/array/data"), data -> {
+        List<TorrentInfo> torrents = CollectionTools.collect(methodResponse.get(0).get(0).get(0).get(0).get(0).children(), data -> {
+            data = data.get(0).get(0);
+            TorrentInfo info = new TorrentInfo();
+            info.setName(data.get(0).get(0).text());
+            info.setHash(data.get(1).get(0).text());
+            info.setSize(Long.parseLong(data.get(2).get(0).text()));
+            info.setDownloaded(Long.parseLong(data.get(3).get(0).text()));
+            info.setDownloadSpeed(Long.parseLong(data.get(4).get(0).text()));
+            info.setUploadSpeed(Long.parseLong(data.get(5).get(0).text()));
+            info.setStatus(TorrentInfo.Status.valueOf("1".equals(data.get(6).get(0).text()), "1".equals(data.get(7).get(0).text()), "1".equals(data.get(8).get(0).text()), "1".equals(data.get(9).get(0).text())));
+            info.setSeeds(Long.parseLong(data.get(10).get(0).text()));
+            info.setPeers(Long.parseLong(data.get(11).get(0).text()));
+            info.setUploaded(Long.parseLong(data.get(12).get(0).text()));
+            return info;
+        });
+
+
+        ScgiClient.XmlRpc.Params params = new ScgiClient.XmlRpc.Params();
+        for (TorrentInfo info : torrents) {
+            params.add(new ScgiClient.XmlRpc("t.multicall", info.getHash(), "d.get_hash=", "t.get_scrape_incomplete=", "t.get_scrape_complete="));
+        }
+
+        response = executeRequest(new ScgiClient.XmlRpc("system.multicall", new ScgiClient.XmlRpc.Params().add(params)));
+        Node multi = parser.parse(response);
+        CollectionTools.eachWithIndex(multi.get(0).get(0).get(0).get(0).get(0).children(), (i, node) -> {
+            int seeds = 0;
+            int peers = 0;
+            for (Node values : node.get(0).get(0).get(0).get(0).get(0).children()) {
+                peers += Integer.parseInt(values.get(0).get(0).get(0).text());
+                seeds += Integer.parseInt(values.get(0).get(0).get(1).text());
+            }
+            torrents.get(i).setTotalSeeds(seeds);
+            torrents.get(i).setTotalPeers(peers);
+            return null;
+        });
+        return torrents;
+    }
+
+    private String executeRequest(ScgiClient.XmlRpc request) {
+        return new ScgiClient.Request(host, port, request.render()).get();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String host = args.length > 0 ? args[0] : "localhost";
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 5000;
+        for (int i = 0; i < 10; i++) {
+
+//        ScgiClient.XmlRpc.Params params = new ScgiClient.XmlRpc.Params();
+//        List<ScgiClient.XmlRpc.Params> list = new ArrayList<>();
+            long time = System.currentTimeMillis();
+            List<TorrentInfo> torrents = new RTorrentClient(host, port).getTorrents();
+            time = System.currentTimeMillis() - time;
+            for (TorrentInfo info : torrents) {
+                System.out.println(info);
+//            params.add(new ScgiClient.XmlRpc("t.multicall", info.getHash(), "d.get_hash=", "t.get_scrape_incomplete="));
+            }
+
+            System.out.println("get torrents info: " + time + " ms");
+            System.out.println("");
+
+            Thread.sleep(1000);
+        }
+//        String request = new ScgiClient.XmlRpc(host, params).render();
+//        System.out.println(new ScgiClient.Request(host, port, request).get());
+
+
+//        if (args.length > 2) {
+//            String[] moreArgs = args.length > 3 ? Arrays.copyOfRange(args, 3, args.length) : new String[0];
+//            String request = new ScgiClient.XmlRpc(args[2], moreArgs).render();
+//            System.out.println(new ScgiClient.Request(host, port, request).get());
+//        }
+    }
+}
---
+++ src/main/java/com/wizzardo/jrt/TorrentInfo.java
@@ -0,0 +1,164 @@
+package com.wizzardo.jrt;
+
+/**
+ * Created by wizzardo on 03.10.15.
+ */
+public class TorrentInfo {
+    private String name;
+    private String hash;
+    private long size;
+    private long downloaded;
+    private long uploaded;
+    private long downloadSpeed;
+    private long uploadSpeed;
+    private long seeds;
+    private long peers;
+    private Status status;
+    private int totalSeeds;
+    private int totalPeers;
+
+    enum Status {
+        SEEDING, FINISHED, DOWNLOADING, STOPPED, PAUSED, CHECKING, UNKNOWN;
+
+        public static Status valueOf(boolean complete, boolean isOpen, boolean isHashChecking, boolean state) {
+            if (complete && isOpen && !isHashChecking && state) {
+                return SEEDING;
+            } else if (complete && !isOpen && !isHashChecking && !state) {
+                return FINISHED;
+            } else if (!complete && isOpen && !isHashChecking && state) {
+                return DOWNLOADING;
+            } else if (!complete && !isOpen && !isHashChecking && state) {
+                // stopped in the middle
+                return STOPPED;
+            } else if (!complete && !isOpen && !isHashChecking && !state) {
+                // i dont know stopped
+                return STOPPED;
+            } else if (!complete && isOpen && !isHashChecking && !state) {
+                return PAUSED;
+            } else if (complete && isOpen && !isHashChecking && !state) {
+                // seeding pause
+                return PAUSED;
+            } else if (complete && !isOpen && !isHashChecking && state) {
+                return FINISHED;
+            } else if (isHashChecking) {
+                return CHECKING;
+            }
+
+            return UNKNOWN;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public long getDownloaded() {
+        return downloaded;
+    }
+
+    public void setDownloaded(long downloaded) {
+        this.downloaded = downloaded;
+    }
+
+    public long getDownloadSpeed() {
+        return downloadSpeed;
+    }
+
+    public void setDownloadSpeed(long downloadSpeed) {
+        this.downloadSpeed = downloadSpeed;
+    }
+
+    public long getUploadSpeed() {
+        return uploadSpeed;
+    }
+
+    public void setUploadSpeed(long uploadSpeed) {
+        this.uploadSpeed = uploadSpeed;
+    }
+
+    public long getSeeds() {
+        return seeds;
+    }
+
+    public void setSeeds(long seeds) {
+        this.seeds = seeds;
+    }
+
+    public long getPeers() {
+        return peers;
+    }
+
+    public void setPeers(long peers) {
+        this.peers = peers;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public long getUploaded() {
+        return uploaded;
+    }
+
+    public void setUploaded(long uploaded) {
+        this.uploaded = uploaded;
+    }
+
+    public int getTotalSeeds() {
+        return totalSeeds;
+    }
+
+    public void setTotalSeeds(int totalSeeds) {
+        this.totalSeeds = totalSeeds;
+    }
+
+    public int getTotalPeers() {
+        return totalPeers;
+    }
+
+    public void setTotalPeers(int totalPeers) {
+        this.totalPeers = totalPeers;
+    }
+
+    @Override
+    public String toString() {
+        return "TorrentInfo{" +
+                "name='" + name + '\'' +
+                ", hash='" + hash + '\'' +
+                ", size=" + size +
+                ", downloaded=" + downloaded +
+                ", uploaded=" + uploaded +
+                ", downloadSpeed=" + downloadSpeed +
+                ", uploadSpeed=" + uploadSpeed +
+                ", seeds=" + seeds +
+                ", peers=" + peers +
+                ", status=" + status +
+                ", totalSeeds=" + totalSeeds +
+                ", totalPeers=" + totalPeers +
+                '}';
+    }
+}
