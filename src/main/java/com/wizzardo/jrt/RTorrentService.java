b349344eb9ff92e46406a05ff7560dfac101d926
wizzardo <wizzardobox@gmail.com>
2016-01-09 00:37:54 +0300

    adc updater thread

--- src/main/java/com/wizzardo/jrt/RTorrentService.java
+++ src/main/java/com/wizzardo/jrt/RTorrentService.java
@@ -1,5 +1,7 @@
 package com.wizzardo.jrt;
 
+import com.wizzardo.epoll.ByteBufferProvider;
+import com.wizzardo.epoll.ByteBufferWrapper;
 import com.wizzardo.http.framework.Holders;
 import com.wizzardo.http.framework.di.Service;
 import com.wizzardo.tools.evaluation.Config;
@@ -15,12 +17,16 @@ import java.util.Map;
 public class RTorrentService implements Service {
 
     private RTorrentClient client;
+    protected Updater updater;
+    AppWebSocketHandler appWebSocketHandler;
 
     public RTorrentService() {
         Config rtorrentConfig = Holders.getConfig().config("jrt").config("rtorrent");
         String host = rtorrentConfig.get("host", "localhost");
         int port = rtorrentConfig.get("port", 5000);
         client = new RTorrentClient(host, port);
+        updater = new Updater(this);
+        updater.start();
     }
 
     public List<TorrentInfo> list() {
@@ -45,5 +51,107 @@ public class RTorrentService implements Service {
 
     public void stop(String torrent) {
         client.stop(torrent);
+    }
+
+    public void pauseUpdater() {
+        updater.pause();
+    }
+
+    public void resumeUpdater() {
+        updater.unpause();
+    }
+
+    protected static class Updater extends Thread implements ByteBufferProvider{
+
+        volatile boolean pause = true;
+        final ByteBufferWrapper buffer = new ByteBufferWrapper(1024 * 50);
+        final RTorrentService rTorrentService;
+
+        Map<String, TorrentInfo> torrents = new HashMap<>();
+
+
+        public Updater(RTorrentService rTorrentService) {
+            this.rTorrentService = rTorrentService;
+            setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                while (pause) {
+                    synchronized (this) {
+                        while (pause) {
+                            try {
+                                this.wait();
+                            } catch (InterruptedException ignored) {
+                            }
+                        }
+                    }
+                }
+
+                try {
+                    check();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }
+
+        protected void check() {
+            List<TorrentInfo> list = rTorrentService.list();
+            for (TorrentInfo info : list) {
+                TorrentInfo old = torrents.get(info.getHash());
+                if (old == null || isUpdated(old, info)) {
+                    rTorrentService.appWebSocketHandler.onUpdate(info);
+                    torrents.put(info.getHash(), info);
+                }
+            }
+        }
+
+        protected boolean isUpdated(TorrentInfo o, TorrentInfo n) {
+            if (o.getStatus() != n.getStatus())
+                return true;
+            if (o.getDownloaded() != n.getDownloaded())
+                return true;
+            if (o.getDownloadSpeed() != n.getDownloadSpeed())
+                return true;
+            if (o.getUploaded() != n.getUploaded())
+                return true;
+            if (o.getUploadSpeed() != n.getUploadSpeed())
+                return true;
+            if (o.getPeers() != n.getPeers())
+                return true;
+            if (o.getSeeds() != n.getSeeds())
+                return true;
+            if (o.getTotalPeers() != n.getTotalPeers())
+                return true;
+            if (o.getTotalSeeds() != n.getTotalSeeds())
+                return true;
+
+            return false;
+        }
+
+        public void pause() {
+            pause = true;
+            System.out.println("updater paused");
+        }
+
+        public void unpause() {
+            synchronized (this) {
+                pause = false;
+                this.notifyAll();
+            }
+            System.out.println("updater unpaused");
+        }
+
+        @Override
+        public ByteBufferWrapper getBuffer() {
+            return buffer;
+        }
     }
 }
